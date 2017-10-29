b0836a6d7f4390711dfb027476d2d7c0855d8b8e
mini-frog <287124371@qq.com>
2017-10-29 14:48:11 +0800

    correct migration class name

--- database/migrations/2017_10_28_144914_create_techGroups.php
+++ database/migrations/2017_10_28_144914_create_techGroups.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateTechGroup extends Migration
+class CreateTechGroups extends Migration
 {
     /**
      * Run the migrations.
--- database/migrations/2017_10_28_145214_create_directors.php
+++ database/migrations/2017_10_28_145214_create_directors.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateDirector extends Migration
+class CreateDirectors extends Migration
 {
     /**
      * Run the migrations.
